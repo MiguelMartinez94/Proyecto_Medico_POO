ee2186854b1b8bfacd8f94736eb2d039cf2b565f
Tex558 <122042676@upq.edu.mx>
2025-06-20 21:42:46 -0600

    Login y botón
    Añadi el codigo html del login (sin imagenes), y el boton con el enlace a la pagina principal de medicos (que tendrá los enlaces a las demas pestañas), añadi una ruta para /medicos y las dos rutas basicas de manejo de excepciones (404 y 405), la mayor parte del codigo del .app esta comentado para que todos sepamos donde añadir que el html de medicos esta vacio y el de login lo comentaré en canto tenga una version final en caso de añadir un logo o imagen

--- Proyecto/MediCenterApp.py
+++ Proyecto/MediCenterApp.py
@@ -1,3 +1,6 @@
+#Proyecto de POO MediCenter
+#Aqui se añaden imports
+
 from flask import Flask, jsonify, render_template, request, url_for, flash, redirect
 
 #Aqui se añaden los app
@@ -7,10 +10,23 @@ app.secret_key ='mysecretkey'
 
 #Apartir de aqui se añaden rutas
 
-@app.route('/')
+@app.route('/') #Ruta principal
 def home():
     return render_template('login.html')
 
-#fin del apartado para añadir rutas
+@app.route('/medicos') #Ruta medicos
+def consulta():
+    return render_template('medicos.html')
+
+@app.errorhandler(404) #Ruta try-catch 404
+def PagNoE(e):
+    return 'EY! Aprenda a escribir :)'
+
+@app.errorhandler(405) #Ruta try-catch 405
+def PagNoE(e):
+    return 'EY! Revisa el metodo de envio :)'
+
+#Fin del apartado para añadir rutas
+
 if __name__ == '__main__':
     app.run(port = 3000, debug = True)
